a2f58996ef7b7a28fe6a77e88ee4281f92b887ee
Aaron Stacy <aaronj1335@staceserve.(none)>
2010-04-14 14:48:12 -0500

    Added a test to remove links if present in home folder files

--- copy_files_to_home_folder.sh
+++ copy_files_to_home_folder.sh
@@ -1,17 +1,22 @@
 #! /bin/bash
 
 [ -f $HOME/.bashrc ] && rm $HOME/.bashrc
+[ -h $HOME/.bashrc ] && rm $HOME/.bashrc
 ln -s $(pwd)/$(dirname $0)/bashrc $HOME/.bashrc
 
 [ -f $HOME/.vimrc ] && rm $HOME/.vimrc
+[ -h $HOME/.vimrc ] && rm $HOME/.vimrc
 ln -s $(pwd)/$(dirname $0)/vimrc $HOME/.vimrc
 
 [ -f $HOME/.vim ] && rm $HOME/.vim
 [ -d $HOME/.vim ] && rm -rf $HOME/.vim
+[ -h $HOME/.vim ] && rm -rf $HOME/.vim
 ln -s $(pwd)/$(dirname $0)/vim $HOME/.vim
 
 [ -f $HOME/.pythonrc ] && rm $HOME/.pythonrc
+[ -h $HOME/.pythonrc ] && rm $HOME/.pythonrc
 ln -s $(pwd)/$(dirname $0)/pythonrc $HOME/.pythonrc
 
 [ -f $HOME/.ssh/id_rsa.pub ] && rm $HOME/.ssh/id_rsa.pub
+[ -h $HOME/.ssh/id_rsa.pub ] && rm $HOME/.ssh/id_rsa.pub
 cp $(pwd)/$(dirname $0)/id_rsa.pub $HOME/.ssh/
