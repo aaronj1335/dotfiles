a2ff187cd498fdf3183211785f13f738622c1c91
Aaron Stacy <aaron.r.stacy@gmail.com>
2010-11-11 10:10:59 -0600

    moved 'home' to 'bin' and updated scripts

--- copy_files_to_home_folder.sh
+++ copy_files_to_home_folder.sh
@@ -1,35 +1,29 @@
 #! /bin/bash
 
-[ -f $HOME/.bashrc ] && rm $HOME/.bashrc
-[ -h $HOME/.bashrc ] && rm $HOME/.bashrc
-ln -s $(pwd)/$(dirname $0)/bashrc $HOME/.bashrc
+function remove_references {
+  [ -f "$1" ] && rm "$1"
+  [ -h "$1" ] && rm "$1"
+  [ -d "$1" ] && rm "$1"
+}
 
-[ -f $HOME/.git-completion.sh ] && rm $HOME/.git-completion.sh
-[ -h $HOME/.git-completion.sh ] && rm $HOME/.git-completion.sh
-ln -s $(pwd)/$(dirname $0)/git-completion.sh $HOME/.git-completion.sh
+remove_references "$HOME/.bashrc"
+ln -s $(pwd)/$(dirname $0)/home_files/bashrc $HOME/.bashrc
 
-[ -f $HOME/.vimrc ] && rm $HOME/.vimrc
-[ -h $HOME/.vimrc ] && rm $HOME/.vimrc
-ln -s $(pwd)/$(dirname $0)/vimrc $HOME/.vimrc
+remove_references "$HOME/.vimrc"
+ln -s $(pwd)/$(dirname $0)/home_files/vimrc $HOME/.vimrc
 
-[ -f $HOME/.gvimrc ] && rm $HOME/.gvimrc
-[ -h $HOME/.gvimrc ] && rm $HOME/.gvimrc
-ln -s $(pwd)/$(dirname $0)/gvimrc $HOME/.gvimrc
+remove_references "$HOME/.gvimrc"
+ln -s $(pwd)/$(dirname $0)/home_files/gvimrc $HOME/.gvimrc
 
-[ -f $HOME/.vim ] && rm $HOME/.vim
-[ -d $HOME/.vim ] && rm -rf $HOME/.vim
-[ -h $HOME/.vim ] && rm -rf $HOME/.vim
-ln -s $(pwd)/$(dirname $0)/vim $HOME/.vim
+remove_references "$HOME/.vim"
+ln -s $(pwd)/$(dirname $0)/home_files/vim $HOME/.vim
 
-[ -f $HOME/.pythonrc ] && rm $HOME/.pythonrc
-[ -h $HOME/.pythonrc ] && rm $HOME/.pythonrc
-ln -s $(pwd)/$(dirname $0)/pythonrc $HOME/.pythonrc
+remove_references "$HOME/.pythonrc"
+ln -s $(pwd)/$(dirname $0)/home_files/pythonrc $HOME/.pythonrc
 
-[ -f $HOME/.ssh/config ] && rm $HOME/.ssh/config
-[ -h $HOME/.ssh/config ] && rm $HOME/.ssh/config
-cp $(pwd)/$(dirname $0)/ssh_config $HOME/.ssh/config
+remove_references "$HOME/.ssh/config"
+cp $(pwd)/$(dirname $0)/home_files/ssh_config $HOME/.ssh/config
 
-[ -f $HOME/.gitconfig ] && rm $HOME/.gitconfig
-[ -h $HOME/.gitconfig ] && rm $HOME/.gitconfig
-ln -s $(pwd)/$(dirname $0)/gitconfig $HOME/.gitconfig
+remove_references "$HOME/.gitconfig"
+ln -s $(pwd)/$(dirname $0)/home_files/gitconfig $HOME/.gitconfig
 
