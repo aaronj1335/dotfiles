6e0b81baff4853889663ac17d53dc4190d10e0c6
Aaron Stacy <aaron.r.stacy@gmail.com>
2011-04-18 14:14:46 -0500

    fixed paths in copy_files_to_home_folder.sh

--- copy_files_to_home_folder.sh
+++ copy_files_to_home_folder.sh
@@ -3,18 +3,24 @@
 function remove_references {
   [ -f "$1" ] && rm "$1"
   [ -h "$1" ] && rm "$1"
-  [ -d "$1" ] && rm "$1"
+  [ -d "$1" ] && rm -rf "$1"
 }
+
+home_files_dir="$(dirname `type -p $0`)/home_files"
+
+if ! echo $home_files_dir | egrep "^/.*" &>/dev/null ; then
+	home_files_dir="$(pwd)/${home_files_dir}"
+fi
+
+remove_references "$HOME/.ssh/config"
+cp $home_files_dir/ssh_config $HOME/.ssh/config
 
 for i in bashrc vimrc gvimrc pythonrc gitconfig dir_colors Xdefaults; do
 	remove_references "$HOME/.${i}"
-	ln -s $(pwd)/$(dirname $0)/home_files/${i} $HOME/.${i}
+	ln -s $home_files_dir/${i} $HOME/.${i}
 
 	if [ "bashrc" = "$i" ]; then
 		source $HOME/.bashrc
 	fi
 done
-
-remove_references "$HOME/.ssh/config"
-cp $(pwd)/$(dirname $0)/home_files/ssh_config $HOME/.ssh/config
 
