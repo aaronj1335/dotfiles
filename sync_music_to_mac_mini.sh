2c1e9cbbf67eabe2a7b576f0afb42c39f3f7dfaa
Aaron Stacy <aaron.r.stacy@gmail.com>
2011-09-27 06:22:34 -0500

    updated sync_music_to_mac_mini.sh

--- sync_music_to_mac_mini.sh
+++ sync_music_to_mac_mini.sh
@@ -4,4 +4,6 @@ rsync -aL --progress \
   --exclude Mobile\ Applications \
   --exclude Previous\ iTunes\ Libraries \
   --exclude iTunes\ Music/Podcasts \
+  --exclude iTunes\ Music/Movies \
+  --exclude iTunes\ Music/TV\ Shows \
   ~/Music/iTunes/ partytimeexcellent.local:Music/iTunes/
