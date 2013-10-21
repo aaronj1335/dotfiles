eb2b768157865eeaa00d5449a5a5ecec92517729
Aaron Stacy <aaron.r.stacy@gmail.com>
2013-10-21 17:25:42 -0500

    who needs that crap

--- sync_music_to_mac_mini.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-rsync -aL --progress \
-  --exclude Mobile\ Applications \
-  --exclude Previous\ iTunes\ Libraries \
-  --exclude iTunes\ Music/Podcasts \
-  --exclude iTunes\ Music/Movies \
-  --exclude iTunes\ Music/TV\ Shows \
-  ~/Music/iTunes/ partytimeexcellent.local:Music/iTunes/
--- sync_music_to_work_laptop.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-rsync -aL --progress \
-  --exclude Mobile\ Applications \
-  --exclude Previous\ iTunes\ Libraries \
-  --exclude iTunes\ Music/Podcasts \
-  --exclude Movies \
-  ~/Music/iTunes/ trainwreck.local:Music/iTunes/
