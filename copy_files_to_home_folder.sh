a459376778e47e3f2877119b08cfd3a0736efe9f
Aaron Stacy <aaron.r.stacy@gmail.com>
2016-01-28 13:18:12 -0600

    make intellij symlinkfarm

--- copy_files_to_home_folder.sh
+++ copy_files_to_home_folder.sh
@@ -6,11 +6,28 @@ function remove_references {
   [ -d "$1" ] && rm -rf "$1"
 }
 
+function symlink_farm {
+  src=$1
+  dest=$2
+  symlinks=`find $src -type f | grep -Ev '\.sw[op]$'`
+
+  for f in $symlinks; do
+    relpath=${f#$src/}
+    mkdir -p $dest/`dirname $relpath`
+    [ -f $dest/$relpath ] && rm $dest/$relpath
+    ln -s $f $dest/$relpath
+  done
+}
+
 home_files_dir="$(dirname `type -p $0`)/home_files"
 
+# make it an absolute path
 if ! echo $home_files_dir | egrep "^/.*" &>/dev/null ; then
   home_files_dir="$(pwd)/${home_files_dir}"
 fi
+
+# normalize out any extra ./'s
+home_files_dir=`cd $home_files_dir; pwd`
 
 # the special cases
 remove_references "$HOME/.ssh/config"
@@ -36,4 +53,14 @@ for i in bashrc vimrc gvimrc pythonrc gitconfig dir_colors Xdefaults ackrc \
   if [ "bashrc" = "$i" ]; then
     source $HOME/.bashrc
   fi
+done
+
+if [ "`uname`" = Linux ]; then
+  intellij_dirs="$HOME/.AndroidStudio1.5/config"
+elif [ "`uname`" = Darwin ]; then
+  intellij_dirs="$HOME/Library/Preferences/AndroidStudio1.5"
+fi
+
+for d in $intellij_dirs; do
+  symlink_farm $home_files_dir/intellij $d
 done
